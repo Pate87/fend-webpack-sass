4d77b6fc68caecc3ab1c224dd2fef681854138ea
Patrick Schubert <patrick-schubert@gmx.de>
2020-05-23 18:27:57 +0200

    adding WorkboxPlugin for Service Workers

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,10 +2,16 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
     devtool: 'source-map',
+    // optimization: {
+    //     minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
+    // },
     output: {
         libraryTarget: 'var',
         library: 'Client'
@@ -28,6 +34,8 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new MiniCssExtractPlugin({filename: '[name].css'})
+        new MiniCssExtractPlugin({filename: '[name].css'
+        }),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
